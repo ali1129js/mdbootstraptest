35be3bcaa7aff666dfb4c9e8c0632f404a2f456c
ali1129js <ali1129js@gmail.com>
2018-04-10 19:40:23 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-10T19:15:28+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-10T19:37:41+02:00
+ */
+
+
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Button , Input} from 'mdbreact';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <form>
+
+          <Input  />
+          <div className="text-center">
+            <Button color="primary">Login</Button>
+            <div class="Button">  </div>
+          </div>
+        </form>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-10T19:12:04+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-10T19:12:04+02:00
+ */
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
